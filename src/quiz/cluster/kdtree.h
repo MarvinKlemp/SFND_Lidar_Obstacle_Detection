e60989681744c43ea4330899b3810db8aad6fd45
MarvinKlemp <mail@marvin-klemp.de>
2020-08-10 15:43:24 +0200

    KD-Tree search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -2,6 +2,7 @@
 // Quiz on implementing kd tree
 
 #include "../../render/render.h"
+#include <queue>
 
 
 // Structure to represent node of kd tree
@@ -39,10 +40,60 @@ struct KdTree
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+
+        std::queue<Node*> queue {};
+        queue.push(root);
+        int xy = 0;
+		while (!queue.empty()) {
+            auto node = queue.front();
+
+            if (in_tolerance_box(target, distanceTol, node->point)) {
+                ids.push_back(node->id);
+            }
+
+            // check if target +/- distance is bigger of smaller than the splitting point
+            if (target[xy % 2] + distanceTol >= node->point[xy % 2]) {
+                if (node->right != nullptr) {
+                    queue.push(node->right);
+                }
+            }
+            if (target[xy % 2] - distanceTol <= node->point[xy % 2]) {
+                if (node->left != nullptr) {
+                    queue.push(node->left);
+                }
+            }
+
+            xy += 1;
+            queue.pop();
+		}
+
 		return ids;
 	}
 
 private:
+    bool in_tolerance_box(std::vector<float> box, float distanceTol, std::vector<float> point)
+    {
+        if (
+            in_tolerance(box[0], distanceTol, point[0]) &&
+            in_tolerance(box[1], distanceTol, point[1])) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static bool in_tolerance(float value_center, float distanceTol, float value)
+    {
+        auto value_add = value_center + distanceTol;
+        auto value_sub = value_center - distanceTol;
+
+	    if (value_sub <= value && value_add >= value) {
+	        return true;
+	    }
+
+	    return false;
+    }
+
     void insert_recursive(Node* node, std::vector<float> point, int id, int xy)
     {
 	    const auto reference_value = node->point[xy % 2];
